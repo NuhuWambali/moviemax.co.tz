c64f0b5247feed3511a0dce892a76b0290183495
nuhuwambali2025 <nuhu.wambali@jematech.co.tz>
2026-09-10 20:18:56 +0300

    Fix storage:link in deploy script (public_html renamed to app)

--- deploy-moviemax.sh
+++ deploy-moviemax.sh
@@ -8,7 +8,9 @@ php artisan cache:clear
 php artisan config:clear
 php artisan view:clear
 php artisan view:cache
-chown -R admin:admin storage bootstrap
+chown -R admin:admin storage bootstrap public
 chmod -R 775 storage bootstrap
+rm -f public/storage
+php artisan storage:link
 ln -sfn /home/admin/domains/moviemax.co.tz/app/public /home/admin/domains/moviemax.co.tz/public_html
 echo 'MOVIEMAX deployment complete'
